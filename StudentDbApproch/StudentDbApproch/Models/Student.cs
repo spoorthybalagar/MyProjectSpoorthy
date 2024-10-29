a218217ca9f13ca98d47d23848882b98294bba84
spoorthybalagar <balagarspoorthy@gmail.com>
2024-10-29 14:50:14 +0530

    eighth  commit 1.2

---
+++ StudentDbApproch/StudentDbApproch/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentDbApproch.Models;
+
+public partial class Student
+{
+    public int StudentId { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public DateOnly Dob { get; set; }
+
+    public string Email { get; set; } = null!;
+}
---
+++ StudentDbApproch/StudentDbApproch/Models/StudentManagementContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentDbApproch.Models;
+
+public partial class StudentManagementContext : DbContext
+{
+    public StudentManagementContext()
+    {
+    }
+
+    public StudentManagementContext(DbContextOptions<StudentManagementContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=DESKTOP-GF410M3\\SQLSERVER2022;Database=StudentManagement;user Id=sa;password=user123;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.StudentId).HasName("PK__Students__32C52A79723589E6");
+
+            entity.Property(e => e.StudentId)
+                .ValueGeneratedNever()
+                .HasColumnName("StudentID");
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.LastName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
